f061534eb476d9355b2f759398e1a10176f729c2
Vitaly Okhrimenko <vitaliy.okhrimenko@sbermarket.ru>
2022-05-01 15:22:08 +0300

    initial

---
+++ Layout Composition/Layout Composition/Layout_CompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_CompositionApp.swift
+//  Layout Composition
+//
+//  Created by Виталий Охрименко on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Layout_CompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
